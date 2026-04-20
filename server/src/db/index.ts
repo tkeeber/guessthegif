35d97beb723cbff3c1d7320ba5c3f0c5be64eb52
[name] <[email]>
2026-04-20 15:52:00 +0100

    ssl db problem

--- server/src/db/index.ts
+++ server/src/db/index.ts
@@ -2,6 +2,9 @@ import { Pool } from 'pg';
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
+  ssl: process.env.NODE_ENV === 'production'
+    ? { rejectUnauthorized: false }
+    : false,
 });
 
 export default pool;
